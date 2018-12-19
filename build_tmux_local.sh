106525ca55bf401cb84de595236d9f80622eb191
Lin <dingjun.lin@softbankrobotics.com>
2018-12-19 12:54:33 +0900

    upload build_tmux_local.sh

---
+++ build_tmux_local.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+# Script for installing tmux on systems where you don't have root access.
+# tmux will be installed in $HOME/local/bin.
+# It's assumed that wget and a C/C++ compiler are installed.
+
+# exit on error
+set -e
+
+TMUX_VERSION=2.8
+
+# create our directories
+mkdir -p $HOME/local $HOME/tmux_tmp
+cd $HOME/tmux_tmp
+
+get_source_files()
+{
+  wget https://github.com/tmux/tmux/releases/download/${TMUX_VERSION}/tmux-${TMUX_VERSION}.tar.gz
+  wget https://github.com/libevent/libevent/releases/download/release-2.1.8-stable/libevent-2.1.8-stable.tar.gz
+  wget https://invisible-mirror.net/archives/ncurses/ncurses-6.1.tar.gz
+}
+
+compile_file()
+{
+  ############
+  # libevent #
+  ############
+  tar xvzf libevent-2.1.8-stable.tar.gz
+  cd libevent-2.1.8-stable
+  ./configure --prefix=$HOME/local --disable-shared
+  make
+  make install
+  cd ..
+
+  ############
+  # ncurses  #
+  ############
+  tar xvzf ncurses-6.1.tar.gz
+  cd ncurses-6.1
+  ./configure --prefix=$HOME/local
+  make
+  make install
+  cd ..
+
+  ############
+  # tmux     #
+  ############
+  tar xvzf tmux-${TMUX_VERSION}.tar.gz
+  cd tmux-${TMUX_VERSION}
+  ./configure CFLAGS="-I$HOME/local/include -I$HOME/local/include/ncurses" LDFLAGS="-L$HOME/local/lib -L$HOME/local/include/ncurses -L$HOME/local/include"
+  CPPFLAGS="-I$HOME/local/include -I$HOME/local/include/ncurses" LDFLAGS="-static -L$HOME/local/include -L$HOME/local/include/ncurses -L$HOME/local/lib" make
+  cp tmux $HOME/local/bin
+  cd ..
+}
+
+# download source files for tmux, libevent, and ncurses
+get_source_files
+# extract files, configure, and compile
+compile_file
+# cleanup
+rm -rf $HOME/tmux_tmp
+
+echo "$HOME/local/bin/tmux is now available. You can optionally add $HOME/local/bin to your PATH."
